dcd4c6f4f0467f8d6b5bcd1c2fa7fb74220a082a
OrelGenya <orelgenya@gmail.com>
2015-09-16 11:49:42 +0300

    upgrade feature test

--- javaee/servlet/src/main/java/tv/livecoding/javastack/servlet/UpgradeServlet.java
+++ javaee/servlet/src/main/java/tv/livecoding/javastack/servlet/UpgradeServlet.java
@@ -11,24 +11,60 @@ import java.io.IOException;
 /**
  * Created by Eugene Orel on 9/15/2015.
  */
-@WebServlet("up")
+@WebServlet("/up")
 public class UpgradeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.upgrade(UpgradeHandler.class);
-        resp.getWriter().print("Upgrade complete!");
+        resp.getWriter().print("Upgrade complete!\r\n\r\n");
     }
 
-    class UpgradeHandler implements HttpUpgradeHandler {
+    public static class UpgradeHandler implements HttpUpgradeHandler {
 
         @Override
         public void init(WebConnection wc) {
             System.out.println("UpgradeHandler inited!");
-            try (ServletInputStream sis = wc.getInputStream();
-                    ServletOutputStream sos = wc.getOutputStream();) {
-                 sis.setReadListener(new UpgradeListener(sis, sos));
+            StringBuilder sb = new StringBuilder();
+            final byte[] bb = new byte[256];
+            int len;
+
+            try {
+                ServletInputStream sis = wc.getInputStream();
+                ServletOutputStream sos = wc.getOutputStream();
+                boolean end = false;
+                while (!end) {
+                    while (!sb.toString().endsWith("\r\n\r\n") && (len = sis.read(bb)) != -1) {
+                        sb.append(new String(bb, 0, len));
+                    }
+                    String cmd = sb.toString();
+                    if (!cmd.endsWith("\r\n\r\n")) {
+                        continue;
+                    }
+                    cmd = cmd.substring(0, cmd.length() - 4);
+                    System.out.println("Command read: " + cmd);
+
+                    switch (cmd) {
+                        case "hey":
+                            sos.write("What's up!\r\n\r\n".getBytes());
+                            break;
+                        case "bye":
+                            sos.write("See you!\r\n\r\n".getBytes());
+                            end = true;
+                            break;
+                        default:
+                            sos.write(cmd.getBytes());
+                            break;
+                    }
+                    sb.setLength(0);
+                }
             } catch (IOException ex) {
-                System.out.println(ex);
+                ex.printStackTrace();
+            } finally {
+                try {
+                    wc.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -38,9 +74,11 @@ public class UpgradeServlet extends HttpServlet {
         }
     }
 
-    class UpgradeListener implements ReadListener {
+    public static class UpgradeListener implements ReadListener {
         final ServletInputStream sis;
         final ServletOutputStream sos;
+
+        final StringBuilder sb = new StringBuilder();
         final byte[] bb = new byte[256];
 
         public UpgradeListener(ServletInputStream sis, ServletOutputStream sos) {
@@ -50,18 +88,29 @@ public class UpgradeServlet extends HttpServlet {
 
         @Override
         public void onDataAvailable() throws IOException {
-            int len = 0;
-            while (sis.isReady()) {
-                len = sis.read(bb, len, bb.length - len);
-//                if (len > 0) {
-//
-//                }
+            int len;
+            while (sis.isReady() && (len = sis.read(bb)) != -1) {
+                sb.append(new String(bb, 0, len));
             }
-            String cmd = new String(bb, "UTF-8");
+            String cmd = sb.toString();
             System.out.println("Command read: " + cmd);
-//            if (cmd.equals("end")) {
-//            }
-            sos.write(bb);
+            if (!cmd.endsWith("\r\n\r\n")) {
+                return;
+            }
+            cmd = cmd.substring(0, cmd.length() - 4);
+            System.out.println("Command read2: " + cmd);
+
+            switch (cmd) {
+                case "hey":
+                    sos.write("What's up!\r\n\r\n".getBytes());
+                    break;
+                case "bye":
+                    sos.write("See you!\r\n\r\n".getBytes());
+                    break;
+                default:
+                    sos.write(cmd.getBytes());
+                    break;
+            }
         }
 
         @Override
--- javaee/servlet/src/test/java/tv/livecoding/javastack/servlet/UpgradeServletTest.java
+++ javaee/servlet/src/test/java/tv/livecoding/javastack/servlet/UpgradeServletTest.java
@@ -5,13 +5,19 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Eugene Orel on 9/15/2015.
@@ -33,9 +39,39 @@ public class UpgradeServletTest {
     }
 
     @Test
-    @Ignore
+//    @Ignore
     public void testUpgrade() throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL(base + "up").openConnection();
-        connection.connect();
+//        System.out.println(base.getHost());
+//        System.out.println(base.getPath());
+//        System.out.println(base);
+        Socket socket = new Socket(base.getHost(), base.getPort());
+        try (InputStream is = socket.getInputStream();
+             OutputStream os = socket.getOutputStream()) {
+            writeCommand(os, "GET " + base.getPath() + "up HTTP/1.1\r\nConnection: upgrade\r\n\r\n");
+
+            String response = readResponse(is);
+            assertTrue(response.startsWith("HTTP/1.1 101 Switching Protocols\r\n"));
+
+            writeCommand(os, "hey\r\n\r\n");
+            assertEquals("What's up!\r\n\r\n", readResponse(is));
+
+            writeCommand(os, "bye\r\n\r\n");
+            assertEquals("See you!\r\n\r\n", readResponse(is));
+        }
+    }
+
+    private void writeCommand(OutputStream os, String cmd) throws IOException {
+        os.write(cmd.getBytes());
+        os.flush();
+    }
+
+    private String readResponse(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        byte[] bb = new byte[256];
+        int len;
+        while(!sb.toString().endsWith("\r\n\r\n") && (len = is.read(bb)) != -1) { //!sb.toString().endsWith("\r\n\r\n") &&
+            sb.append(new String(bb, 0, len));
+        }
+        return sb.toString();
     }
 }
